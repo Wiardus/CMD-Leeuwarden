fc0396df68c01f1750e42695ec0bfbc57c8131ac
Stijn <stijn.timmerman@student.nhlstenden.com>
2021-11-03 15:06:20 +0100

    3D test toegevoegd

--- Index.js
+++ Index.js
@@ -1,5 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
+import {App} from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+
+'use strict';
+
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+const domContainer = document.querySelector('#react_container');
+ReactDOM.render(e(LikeButton, <App />), domContainer);
